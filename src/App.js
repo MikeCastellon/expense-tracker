44c222e888ea8a511df435a7d83da5e4f43ad38c
Mike <mike.castellon5@gmail.com>
2021-01-02 11:57:32 -0500

    added main component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core';
+
+import useStyles from './styles';
 
 import Details from './components/details/details';
+import Main from './components/main/main';
 
 const App = () => {
+    const classes = useStyles()
     return (
         <div>
-            <Grid container spacing={0} alignItems="center" justify='center' style={{ height: '100vh' }}>
-                <Grid item xs={12} sm={4}>
-                    <Details />
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify='center' style={{ height: '100vh' }}>
+                <Grid   item xs={12} sm={4}>
+                    <Details title='Income' />
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    Main
+                    <Main />
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <Details />
+                    <Details title='Expenses' />
                 </Grid>
             </Grid>
         </div>
---
+++ src/components/main/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import useStyles from './styles';
+
+import { Card, CardHeader, Typography, CardContent, Grid, Divider } from '@material-ui/core';
+
+
+const Main = () => {
+    const classes = useStyles();
+    return (
+       <Card className={classes.root}>
+            <CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
+            <CardContent>
+                <Typography align='center' variant='h5'> Total Balance $100</Typography>
+                <Typography variant='subtitle1' style={{ lineHeight:'1.5em', marginTop: '20px' }}>
+                    Try Saying: Add income to for $100 category salary for Monday... 
+                </Typography>
+                <Divider />
+        </CardContent>
+        <CardContent className={classes.cardContent}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                </Grid>
+            </Grid>
+        </CardContent>
+       </Card>
+    )
+}
+
+export default Main
