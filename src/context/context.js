e8dd178a573938f681557b296bfc43fbd30368bb
Mike <mike.castellon5@gmail.com>
2021-01-02 19:31:33 -0500

    set up context api and reducer

---
+++ src/context/context.js
@@ -0,0 +1,25 @@
+import React, { useReducer, createContext } from 'react';
+
+import contextReducer from './contextReducer';
+
+const initialState = []
+
+export const ExpenseTrackerContext = createContext(initialState)
+
+export const Provider = ({ children }) => {
+    const [ transactions, dispatch ] = useReducer(contextReducer, initialState)
+
+    const deleteTransactions = (id) =>  dispatch({ type: 'DELETE_TRANSACTION', payload: id })
+    
+
+    const addTransactions = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction })
+    
+    return (
+        <ExpenseTrackerContext value={{ 
+            deleteTransactions,
+            addTransactions
+        }}>
+            {children}
+        </ExpenseTrackerContext>
+    )
+} 
